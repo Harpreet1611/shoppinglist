ade5bc662781845225cfef3430520a86ce87f5b4
Harpreet <hpkaur889@gmail.com>
2023-04-21 17:35:48 -0400

    last

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ const App = ({ signOut }) => {
       <Image
         src={note.image}
         alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
+        style={{ width: 200 }}
       />
     )}
     <Button variation="link" onClick={() => deleteNote(note)}>
